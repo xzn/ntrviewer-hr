4806d602998ff898f64e94aa0a29b29cde4aee8b
JS Deck <jsdeckerido@gmail.com>
2024-09-07 23:51:44 -0300

    parentheses in macros

--- rp_syn.h
+++ rp_syn.h
@@ -9,17 +9,17 @@
 typedef pthread_mutex_t rp_lock_t;
 typedef sem_t rp_sem_t;
 
-#define rp_lock_init(n) pthread_mutex_init(&n, 0)
-#define rp_lock_wait_try(n) pthread_mutex_trylock(&n)
-#define rp_lock_wait(n, to) pthread_mutex_timedlock(&n, to)
-#define rp_lock_rel(n) pthread_mutex_unlock(&n)
-#define rp_lock_close(n) pthread_mutex_destroy(&n)
+#define rp_lock_init(n) pthread_mutex_init(&(n), 0)
+#define rp_lock_wait_try(n) pthread_mutex_trylock(&(n))
+#define rp_lock_wait(n, to) pthread_mutex_timedlock(&(n), to)
+#define rp_lock_rel(n) pthread_mutex_unlock(&(n))
+#define rp_lock_close(n) pthread_mutex_destroy(&(n))
 
-#define rp_sem_init(n, i) sem_init(&n, 0, i)
-#define rp_sem_wait_try(n) ({ int _ret = sem_trywait(&n); if (_ret) { _ret = errno; if (_ret == EAGAIN) { _ret = ETIMEDOUT; }} _ret;})
-#define rp_sem_wait(n, to) ({ int _ret = sem_timedwait(&n, to); if (_ret) { _ret = errno; } _ret;})
-#define rp_sem_rel(n) sem_post(&n)
-#define rp_sem_close(n) sem_destroy(&n)
+#define rp_sem_init(n, i) sem_init(&(n), 0, i)
+#define rp_sem_wait_try(n) ({ int _ret = sem_trywait(&(n)); if (_ret) { _ret = errno; if (_ret == EAGAIN) { _ret = ETIMEDOUT; }} _ret;})
+#define rp_sem_wait(n, to) ({ int _ret = sem_timedwait(&(n), to); if (_ret) { _ret = errno; } _ret;})
+#define rp_sem_rel(n) sem_post(&(n))
+#define rp_sem_close(n) sem_destroy(&(n))
 
 struct rp_syn_comp_func_t {
 	rp_sem_t sem;
