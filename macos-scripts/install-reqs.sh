67aad2a47d3156036ec2e1d402ffb4253d59737c
JS Deck <jsdeckerido@gmail.com>
2025-08-11 23:20:23 -0300

    macOS workflow download vulkan sdk

--- macos-scripts/install-reqs.sh
+++ macos-scripts/install-reqs.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-sudo ./vulkansdk-macOS-$VULKAN_VERSION.app/Contents/MacOS/vulkansdk-macOS-$VULKAN_VERSION --root ~/VulkanSDK/$VULKAN_VERSION --accept-licenses --default-answer --confirm-command install com.lunarg.vulkan.core com.lunarg.vulkan.usr
-
 brew update
-brew install sdl3 libplacebo
+brew install sdl3 libplacebo wget
+
+wget https://sdk.lunarg.com/sdk/download/$VULKAN_VERSION/mac/vulkansdk-macos-$VULKAN_VERSION.zip
+unzip vulkansdk-macos-$VULKAN_VERSION.zip
+sudo ./vulkansdk-macOS-$VULKAN_VERSION.app/Contents/MacOS/vulkansdk-macOS-$VULKAN_VERSION --root ~/VulkanSDK/$VULKAN_VERSION --accept-licenses --default-answer --confirm-command install com.lunarg.vulkan.core com.lunarg.vulkan.usr
