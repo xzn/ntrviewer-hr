bf0e93cae02858a390e87819cbfe581f34180fa5
JS Deck <jsdeckerido@gmail.com>
2025-08-12 00:12:46 -0300

    fix ln path macos workflow

--- macos-scripts/install-reqs.sh
+++ macos-scripts/install-reqs.sh
@@ -10,9 +10,9 @@ rustup default nightly-2025-08-08
 pushd librashader/librashader-capi
 cargo build --profile optimized --no-default-features --features runtime-vulkan,runtime-metal
 popd
-ln -s librashader/include/librashader.h ntrviewer-hr/include/librashader.h
+ln -s ../../librashader/include/librashader.h ntrviewer-hr/include/librashader.h
 mkdir ntrviewer-hr/lib
-ln -s librashader/target/optimized/liblibrashader_capi.dylib ntrviewer-hr/lib/librashader.dylib
+ln -s ../../librashader/target/optimized/liblibrashader_capi.dylib ntrviewer-hr/lib/librashader.dylib
 
 wget -nv https://sdk.lunarg.com/sdk/download/$VULKAN_VERSION/mac/vulkansdk-macos-$VULKAN_VERSION.zip
 unzip vulkansdk-macos-$VULKAN_VERSION.zip
