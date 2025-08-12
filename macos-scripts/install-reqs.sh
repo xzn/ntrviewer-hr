22303d7c3c672772058dc0bfea431506336ad024
JS Deck <jsdeckerido@gmail.com>
2025-08-11 23:30:22 -0300

    librashader dep macos workflow

--- macos-scripts/install-reqs.sh
+++ macos-scripts/install-reqs.sh
@@ -1,7 +1,15 @@
 #!/usr/bin/env bash
 
 brew update
-brew install sdl3 libplacebo wget
+brew install sdl3 libplacebo wget rustup
+
+rustup toolchain install nightly-2025-08-08
+pushd librashader
+cargo run -p librashader-build-script -- --profile optimized
+popd
+ln -s librashader/include/librashader.h ntrviewer-hr/include/librashader.h
+mkdir ntrviewer-hr/lib
+ln -s librashader/target/optimized/librashader.dylib ntrviewer-hr/lib/librashader.dylib
 
 wget https://sdk.lunarg.com/sdk/download/$VULKAN_VERSION/mac/vulkansdk-macos-$VULKAN_VERSION.zip
 unzip vulkansdk-macos-$VULKAN_VERSION.zip
