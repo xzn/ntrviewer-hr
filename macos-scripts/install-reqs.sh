7ab89f9514554cb7804719d179b780e721f07410
JS Deck <jsdeckerido@gmail.com>
2025-08-12 00:42:44 -0300

    intel macos workflow try

--- macos-scripts/install-reqs.sh
+++ macos-scripts/install-reqs.sh
@@ -16,4 +16,4 @@ ln -s ../../librashader/target/optimized/liblibrashader_capi.dylib ntrviewer-hr/
 
 wget -nv https://sdk.lunarg.com/sdk/download/$VULKAN_VERSION/mac/vulkansdk-macos-$VULKAN_VERSION.zip
 unzip vulkansdk-macos-$VULKAN_VERSION.zip
-sudo ./vulkansdk-macOS-$VULKAN_VERSION.app/Contents/MacOS/vulkansdk-macOS-$VULKAN_VERSION --root ~/VulkanSDK/$VULKAN_VERSION --accept-licenses --default-answer --confirm-command install com.lunarg.vulkan.core com.lunarg.vulkan.usr
+./vulkansdk-macOS-$VULKAN_VERSION.app/Contents/MacOS/vulkansdk-macOS-$VULKAN_VERSION copy_only=1 --root ~/VulkanSDK/$VULKAN_VERSION --accept-licenses --default-answer --confirm-command install com.lunarg.vulkan.core com.lunarg.vulkan.usr
